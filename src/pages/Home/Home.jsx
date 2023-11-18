47839c13d7efec7b90dfda3b294e2144c5d3cb98
AchaRhaah <acharhaar2003@gmail.com>
2023-11-18 06:13:04 +0100

    .

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,6 +2,6 @@ import React from 'react'
 
 export default function Home() {
   return (
-    <div className='bg-red-500'>Home</div>
+    <div className='bg-red-500'>Let the battle begin!!!!</div>
   )
 }
